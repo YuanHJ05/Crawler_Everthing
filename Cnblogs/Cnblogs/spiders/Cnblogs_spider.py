6cbb15ed6f851f522bcac95146109819b8630a90
yuanHJ <yuanhj@outlook.com>
2019-10-15 13:40:50 +0800

    爬取博客园python的文章--修改了爬取方式

--- Cnblogs/Cnblogs/spiders/Cnblogs_spider.py
+++ Cnblogs/Cnblogs/spiders/Cnblogs_spider.py
@@ -6,14 +6,17 @@ from ..items import CnblogsItem
 class CnblogsSpiderSpider(scrapy.Spider):
     name = 'Cnblogs_spider'
     allowed_domains = ['www.cnblogs.com']
-    start_urls = ['http://www.cnblogs.com/']
-    base_url = 'https://www.cnblogs.com/cate/python#p{0}'
+    start_urls = ['https://www.cnblogs.com/cate/python#p1']
 
-    def start_requests(self):
-        for i in range(1, 2):
-            yield scrapy.Request(url=self.base_url.format(i), callback=self.parse)
+    # base_url = 'https://www.cnblogs.com/cate/python#p{0}'
+
+    # def start_requests(self):
+    #     for i in range(1, 201):
+    #         print(self.base_url.format(i))
+    #         yield scrapy.Request(url=self.base_url.format(i), callback=self.parse)
 
     def parse(self, response):
+        print(response.url)
         item = CnblogsItem()
         divs = response.xpath("//div[@id='post_list']/div")
         for div in divs:
@@ -24,3 +27,9 @@ class CnblogsSpiderSpider(scrapy.Spider):
             item['pub_date'] = ".".join(
                 div.xpath("./div[@class='post_item_body']/div[@class='post_item_foot']/text()").re("发布于 (.*?) "))
             yield item
+
+        next_word = response.xpath("//div[@id='paging_block']/div[@class='pager']/a[last()]/text()").get()
+        if next_word == "Next >":
+            next_url = response.xpath("//div[@id='paging_block']/div[@class='pager']/a[last()]/@href").get()
+            next_url = "https://www.cnblogs.com" + str(next_url)
+            yield scrapy.Request(url=next_url, callback=self.parse)
